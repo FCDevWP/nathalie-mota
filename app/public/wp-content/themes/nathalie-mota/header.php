4af232d63fe5ab1a44f754fbe70060f6baa2612e
Frederic Claveau <fredclaveau@gmail.com>
2024-06-04 16:23:44 +0200

    creation fichier home.php

--- app/public/wp-content/themes/nathalie-mota/header.php
+++ app/public/wp-content/themes/nathalie-mota/header.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="keywords" content="nathalie mota, photographe professionnelle, événementiel, réception, concert, mariage, télévision, photo argentique, photo numérique"/>
+    <meta name="description" content="Nathalie Mota – photographe professionnelle pour vos événements, réception, concert, mariage, télévision, photo argentique, photo numérique."/>
+
     <title>Nathalie Mota - Photographe</title>
     <?php wp_head(); ?>
 </head>
--- app/public/wp-content/themes/nathalie-mota/index.php
+++ app/public/wp-content/themes/nathalie-mota/index.php
@@ -1,3 +1,2 @@
-<?php get_header(); ?>
-<h1>Bienvenue sur le site de Nathalie Mota !</h1>
-<?php get_footer(); ?>
+<?php
+// Silence is golden.
