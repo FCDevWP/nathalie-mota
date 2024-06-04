ac20e3a3b422d39bf3ecb3d2db0c4e8c9ccb04c0
Frederic Claveau <fredclaveau@gmail.com>
2024-06-04 19:10:30 +0200

    changement style.css

--- app/public/wp-content/themes/nathalie-mota/header.php
+++ app/public/wp-content/themes/nathalie-mota/header.php
@@ -11,3 +11,28 @@
     <?php wp_head(); ?>
 </head>
 <body>
+    <!-- <header id="header" class="header flexrow">
+		<div class="container-header flexrow">
+			<a href="<?php echo home_url( '/' ); ?>" aria-label="Page d'accueil de Nathalie Mota">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo_nathalie_mota.png" 
+				alt="Logo <?php echo bloginfo('name'); ?>">
+			</a>
+			<nav id="navigation">
+				<?php 
+					// Affichage du menu main déclaré dans functions.php
+					wp_nav_menu(array('theme_location' => 'main')); 
+				?>
+				<button id="modal__burger" class="btn-modal" aria-label="Menu pour la version portable">
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                </button>
+				
+                <div id="modal__content" class="modal__content">           
+					<?php 				
+					wp_nav_menu(array('theme_location' => 'main')); 
+					?>
+                </div>
+			</nav>
+		</div>
+	</header>	 -->
