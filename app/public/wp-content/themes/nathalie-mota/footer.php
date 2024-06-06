bcda3e4f3c8328f344cb5c352ec70bcee4d7b174
Frederic Claveau <fredclaveau@gmail.com>
2024-06-06 08:50:57 +0200

    création footer

--- app/public/wp-content/themes/nathalie-mota/footer.php
+++ app/public/wp-content/themes/nathalie-mota/footer.php
@@ -1,9 +1,17 @@
-        
-        
-        
-        
-        
-        
+        <div class="footer-container">
+            <div class="left-space"></div>
+            <div class="nav-footer">
+                <span><a href="#">MENTIONS LEGALES</a></span>
+                <span><a href="#">VIE PRIVÉE</a></span>
+                <span><a href="#">TOUS DROITS RÉSERVÉS</a></span>
+            </div>
+            <div class="right-space"></div>
+        </div>
+
+
+
+
+
         <?php get_template_part('template-parts/modal-contact'); ?>
         <?php wp_footer(); ?>
     </body>
