0e438fb871a6b66e041dd3c171e997a46c9ab698
Frederic Claveau <fredclaveau@gmail.com>
2024-06-11 16:15:22 +0200

    travail sur menus déroulants - structuration mais tjrs pb survol rouge

--- app/public/wp-content/themes/nathalie-mota/home.php
+++ app/public/wp-content/themes/nathalie-mota/home.php
@@ -14,17 +14,17 @@
     <section class="category-general">
       <select name="category" id="category" class="category">
         <option value="" disabled selected>CATÉGORIES</option>
-        <option value="reception" class="reception">Réception</option>
-        <option value="concert" class="concert">Concert</option>
-        <option value="mariage" class="mariage">Mariage</option>
-        <option value="television" class="television">Télévision</option>
+        <option value="reception" class="reception custom-option">Réception</option>
+        <option value="concert" class="concert custom-option">Concert</option>
+        <option value="mariage" class="mariage custom-option">Mariage</option>
+        <option value="television" class="television custom-option">Télévision</option>
       </select>
     </section>
     <section class="format-general">
       <select name="format" id="format" class="format">
         <option value="" disabled selected>FORMAT</option>
-        <option value="paysage" class="paysage">Paysage</option>
-        <option value="portrait" class="portrait">Portrait</option>
+        <option value="paysage" class="paysage custom-option">Paysage</option>
+        <option value="portrait" class="portrait custom-option">Portrait</option>
       </select>
     </section>
   </div>
@@ -32,8 +32,8 @@
     <section class="tri-general">
       <select name="tri" id="tri" class="tri">
         <option value="" disabled selected>TRIER PAR</option>
-        <option value="new" class="new">Nouveautés</option>
-        <option value="old" class="old">Oeuvres anciennes</option>
+        <option value="new" class="new custom-option">Nouveautés</option>
+        <option value="old" class="old custom-option">Oeuvres anciennes</option>
       </select>
     </section>
   </div>
