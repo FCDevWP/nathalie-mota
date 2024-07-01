26895c757ec2a7448ae42fdded3aac75125de226
Frederic Claveau <fredclaveau@gmail.com>
2024-07-01 14:00:11 +0200

    section gauche section 2 terminée

--- app/public/wp-content/themes/nathalie-mota/js/scripts.js
+++ app/public/wp-content/themes/nathalie-mota/js/scripts.js
@@ -1,14 +1,14 @@
 (function($) {
   $(document).ready(function() {
-    // Ouvre la modale lors du clic sur le lien "Contact"
-    $('.open-contact-modal').on('click', function(e) {
+    // Ouvre la modale lors du clic sur le lien "Contact" (existant)
+    $('.open-contact-modal, .btn-contact').on('click', function(e) {
       e.preventDefault();
       $('#contact-modal').fadeIn();
     });
 
     // Ferme la modale lors du clic en dehors de celle-ci
     $(document).on('click', function(e) {
-      if ($(e.target).closest('.modal-content').length === 0 && !$(e.target).hasClass('open-contact-modal')) {
+      if ($(e.target).closest('.modal-content').length === 0 && !$(e.target).hasClass('open-contact-modal') && !$(e.target).hasClass('btn-contact')) {
         $('#contact-modal').fadeOut();
       }
     });
