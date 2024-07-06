1b52c267ca662c78bfb8d103a5367d7fa6172b43
Frederic Claveau <fredclaveau@gmail.com>
2024-07-06 09:46:40 +0200

    ajout pages footer

--- app/public/wp-content/themes/nathalie-mota/footer.php
+++ app/public/wp-content/themes/nathalie-mota/footer.php
@@ -3,9 +3,9 @@
 
         <footer>
             <div class="nav-footer">
-                <span><a href="#">MENTIONS LEGALES</a></span>
-                <span><a href="#">VIE PRIVÉE</a></span>
-                <span><a href="#">TOUS DROITS RÉSERVÉS</a></span>
+                <span><a href="<?php echo get_permalink(get_page_by_path('mentions-legales')); ?>">MENTIONS LEGALES</a></span>
+                <span><a href="<?php echo get_permalink(get_page_by_path('vie-privee')); ?>">VIE PRIVÉE</a></span>
+                <span><a href="<?php echo get_permalink(get_page_by_path('tous-droits-reserves')); ?>">TOUS DROITS RÉSERVÉS</a></span>
             </div>
         </footer>
 
